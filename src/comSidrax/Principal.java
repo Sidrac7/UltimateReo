0511dbaf10c781d04df1841b0c58c22698c3aa3f
Perkles <sidoliver00@gmail.com>
2023-02-22 23:40:57 -0300

    Initial commit

---
+++ src/comSidrax/Principal.java
@@ -0,0 +1,17 @@
+package comSidrax;
+
+import java.util.Collections;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Viva CISTO Rei");
+        int num1 = 10;
+        int num2 = 22;
+        int soma=num1+num2;
+
+        System.out.println("A soma de "+num1+" + "+num2+" = "+soma);
+
+
+
+    }
+}
